532cf6afe0c923e085500ae60dcc91a71092485c
Singh <sabhisheksignh343104@gmail.com>
2023-07-05 20:34:39 +0530

    comments added

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-#!/data/data/com.termux/files/usr/bin/python3
-'''
-This command is for terminal:
-find $HOME -iname ab.py
-'''
-import os
+import os   # importing os module to deal with directories and files
 
-reqf = input("Enter the file to search location: ")
-a = 0
-for r,d,f in os.walk("/data/data/com.termux"):
-	for eachf in f:
-		if eachf == reqf:
-			a += 1
-			print(os.path.join(r,eachf))
+path = input("Main directory/drive path: ")
+requiredFile = input("Enter the file to search location: ")
+
+count= 0
+# walking through this path and all the directorries in this path to search the required file
+for r,d,files in os.walk(path):
+	for file in files:
+		# when file is present in the list of files of current directory
+		if file == requiredFile:
+			count+= 1
+			print(os.path.join(r,file))  # full path of the file
 print()
-if a == 0:
+if count== 0:
 	print("File not found.")
-elif a == 1:
-	print(f"{a} location is found for file named {reqf}")
+elif count== 1:
+	print(f"{count} location is found for file named {requiredFile}")
 else:
-	print(f"Total {a} locations are found for file named {reqf}")
+	print(f"Total {count} locations are found for file named {requiredFile}")
 print()
